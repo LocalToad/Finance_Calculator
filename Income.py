c7bf9d3e4b716a4ba17b4b327738ce4e3bbd02ce
Solidus Snake <snake@dontdox.me>
2025-08-11 02:27:04 -0400

    fixed code coverage

--- Income.py
+++ Income.py
@@ -3,13 +3,19 @@ import pickle
 
 
 #def infoedit(index):
-with open('Dicts.txt', 'rb') as f:  # Open in binary read mode ('rb')
-    user_settings = pickle.load(f)
-users = [toad, snake]
-# = (
-   # {'name':(str), 'salary':(bool), 'wage':(float.00), 'hours':(float.0)},
-    #{'name':'snake', 'salary':True, 'wage':2477.12, 'hours':0}
-#)
+def grabUserSettings():
+    with open('Dicts.txt', 'rb') as f:  # Open in binary read mode ('rb')
+        user_settings = pickle.load(f)
+        return user_settings
+    # = (
+       # {'name':(str), 'salary':(bool), 'wage':(float.00), 'hours':(float.0)},
+        #{'name':'snake', 'salary':True, 'wage':2477.12, 'hours':0}
+    #)
+
+
+# Error cases or success cases reture True-y values
+# Other cases that cause incmain to continue looping return False-y values
+
 def incmain(index, settings_dict):
     salary = settings_dict[index].get('salary')
     isboolean = isinstance(salary, bool)
@@ -33,29 +39,38 @@ def incmain(index, settings_dict):
                 return 402001
         elif cmd.lower() == 'n':
             print("Exiting Calculator")
-            break
+            return True
         elif cmd.lower() == 'edit':
-            print("name(str), salary(bool), wage(float.00), hours(float.0)")
-            feature = input(f"Which feature do you want to edit for {users[index]}? ").lower()
-            print(user_settings[index][feature])
-            a = input("Do you want to change the value?(y/n) ")
-            if a.lower() == 'y':
-                new = input(f"Input the New value for {feature}? ").lower()
-                if feature == 'wage' or feature == 'hours':
-                    new = float(new)
-                elif feature == 'salary':
-                    if new == 'true':
-                        new = True
-                    if new == 'false':
-                        new = False
-                user_settings[index][feature] = new
-                with open('Dicts.txt', 'wb') as f:  # Open in binary write mode ('wb')
-                    pickle.dump(user_settings, f)
-            elif a.lower() == 'n':
-                print("temp done")
+            if not inc_write(index, settings_dict):
+                continue
             else:
-                print("Eror 401080")
-                return 401070
+                # User chose to exit, equivalent to break
+                return True
         else:
             print("Error 401050")
             return 401050
+
+def inc_write(index, settings_dict):
+    print("name(str), salary(bool), wage(float.00), hours(float.0)")
+    feature = input("Which feature do you want to edit? ").lower()
+    print(settings_dict[index][feature])
+    a = input("Do you want to change the value?(y/n) ")
+    if a.lower() == 'y':
+        new = input(f"Input the New value for {feature}? ").lower()
+        if feature == 'wage' or feature == 'hours':
+            new = float(new)
+        elif feature == 'salary':
+            if new == 'true':
+                new = True
+            if new == 'false':
+                new = False
+        settings_dict[index][feature] = new
+        with open('Dicts.txt', 'wb') as f:  # Open in binary write mode ('wb')
+            pickle.dump(settings_dict, f)
+        return False
+    elif a.lower() == 'n':
+        print("temp done")
+        return True
+    else:
+        print("Eror 401080")
+        return 401070
